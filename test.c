748351264d7913038745d4539f8281bcc9b1f21a
Jiyong Kwag <jhkwag97@gmail.com>
2023-01-20 00:28:21 -0500

    EOS tokenization complete

--- test.c
+++ test.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include<string.h> 
+#include<stdlib.h> 
+
 
 enum core {
     // Keywords
@@ -42,14 +45,25 @@ enum core {
     ERROR
 };
 static FILE* fp;
-int i = 0;
+static char* tokenString;
+static int i = 0;
+static int eof=0;
 
 void nextToken(){
     int first=1;
+    int tokenSize=0;
+    tokenString = calloc(20, sizeof(char));
     while(1){
         char ch = fgetc(fp);
 
-        if(ch == EOF) break;
+        if(ch == EOF){
+            if(first){
+                tokenString[0]='E';
+                tokenString[1]='O';
+                tokenString[2]='S';
+            }
+            break;
+        } 
         if(ch == ' ' || ch =='\n'){
             if(first){
                 continue;
@@ -59,20 +73,22 @@ void nextToken(){
         }
         first=0;
 
-        //printf("<%d> %c\n",i, ch);
-        printf("%c",ch);
+
+        tokenString[tokenSize]=ch;
+        tokenSize++;
         i++;
 
     }
-    printf("\n");
+    printf("%s\n", tokenString);
+    free(tokenString);
 
 }
 
 int main(){
 
-    fp = fopen ("1.1.code", "r");
+    fp = fopen ("Correct/1.code", "r");
     int s;
-    for(s=0; s<12;s++){
+    for(s=0; s<14;s++){
         nextToken();
     }
 
